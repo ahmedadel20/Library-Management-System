c7b60b91e9d45f08e338ef19db8a8eaf2c585880
Ahmed Adel <ahmedadel99991@gmail.com>
2024-09-07 20:23:14 +0300

    Added Book and Customer Models

---
+++ Library-Management-System/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library_Management_System.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Author { get; set; }
+
+        [Required]
+        [Url]
+        public string Cover { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Genre { get; set; }
+    }
+}
---
+++ Library-Management-System/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library_Management_System.Models
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
